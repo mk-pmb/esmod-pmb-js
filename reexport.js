1998e65741a1edda6d079c7ae1a0ebccbcd687ec
mk-pmb <mk-pmb@users.noreply.github.com>
2018-08-28 00:01:14 +0200

    Legacy paths don't work for *.mjs. meh.

--- reexport.js
+++ reexport.js
@@ -10,7 +10,14 @@ dfOpt = {
   cjs: {
     // namedExports: true,  // <- seems to have no effect anyway
     // interop: true,       // <- seems to have no effect anyway
-    paths: true,  // support old-school node path rules
+
+    paths: true,  // support old-school node path rules…
+    // NB: … but in *.js only. Not a bug, just red tape (upstream issue 582,
+    //    https://github.com/standard-things/esm/issues/582).
+    //    One stopgap to make it work with .mjs would be to do ALL
+    //    module resolving ourselves (https://git.io/fAOyA)
+    //    but I'll rather research a less intrusive fallback approach
+    //    some day. Beat me to the draw with your PR!
   },
   stripSuffixes: /(?:[\.\-](?:c?js|common|node|bridge))+$/,
   addSuffix: '.mjs',

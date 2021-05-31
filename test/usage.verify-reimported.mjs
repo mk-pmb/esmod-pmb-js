6f7b1dbef8fc92deb8de13139b0c9c89b861b96c
mk-pmb <mk-pmb@users.noreply.github.com>
2021-05-31 05:21:45 +0200

    v0.1.12: Add import test for is-fn v3 which has type=module

--- test/usage.verify-reimported.mjs
+++ test/usage.verify-reimported.mjs
@@ -1,4 +1,6 @@
 import 'usnam-pmb';
+
+import isFunc from 'is-fn';
 import eq from 'equal-pmb';
 import dfOnly from './default-export-only.node';
 import named from './named-exports-only.node';
@@ -7,3 +9,4 @@ import mixed from './usage.node';
 eq(dfOnly, { isDefaultExport: true, bar: 5 });
 eq(named, { answer: 42, foo: 23 });
 eq(mixed, { answer: 42, default: dfOnly, foo: 23 });
+eq(isFunc(eq), true);

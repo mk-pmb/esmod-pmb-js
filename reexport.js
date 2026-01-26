78e0018c638ec82e8006d954ccaaf961b13b39cf
mk-pmb <mk-pmb@users.noreply.github.com>
2026-01-26 05:07:48 +0100

    Prepare for version-dependent default options.

--- reexport.js
+++ reexport.js
@@ -2,7 +2,6 @@
 'use strict';
 
 var EX, adapter, pkgName = require('./package.json').name,
-  dfOpt,
   nodeVerNums = process.versions.node.split('.').map(Number),
   envOpt = require('./envOpt.js'),
   obAss = Object.assign, objHas = Object.prototype.hasOwnProperty;
@@ -15,40 +14,17 @@ adapter = (function nodeVersionsGate() {
 }());
 
 
-dfOpt = {
-  mode: 'auto',
-  cjs: {
-    // namedExports: true,  // <- seems to have no effect anyway
-    // interop: true,       // <- seems to have no effect anyway
-
-    paths: true,  // support old-school node path rules…
-    // NB: … but in *.js only. Not a bug, just red tape (upstream issue 582,
-    //    https://github.com/standard-things/esm/issues/582).
-    //    One stopgap to make it work with .mjs would be to do ALL
-    //    module resolving ourselves (https://git.io/fAOyA)
-    //    but I'll rather research a less intrusive fallback approach
-    //    some day. Beat me to the draw with your PR!
-  },
-  stripSuffixes: /(?:[\.\-](?:c?js|common|node|bridge))+$/,
-  addSuffix: '.mjs',
-  preferDefaultExport: 1,
-  resolveImportedValues: true,
-  reexport: true,
-  debug: (envOpt.debugEsm >= 1),
-};
-
-
 function ifObj(x, d) { return ((x && typeof x) === 'object' ? x : d); }
 
 function subObAss(dest, prop, src) {
-  var ass = obAss({}, (src || dfOpt)[prop], dest[prop]);
+  var ass = obAss({}, (src || EX.defaultConfig)[prop], dest[prop]);
   dest[prop] = ass;
   return ass;
 }
 
 function optimizeOpts(origOpts) {
-  if (!origOpts) { return dfOpt; }
-  var opt = obAss({}, dfOpt, origOpts);
+  if (!origOpts) { return EX.defaultConfig; }
+  var opt = obAss({}, EX.defaultConfig, origOpts);
   if (opt.cjs !== true) { subObAss(opt, 'cjs'); }
   if (envOpt.debugEsm > 9e3) { opt.debug = true; }
   return opt;
@@ -87,7 +63,9 @@ EX = function esmBridge(bridgeModule, opt) {
   }
   return esmRqr;
 };
-EX.defaultConfig = dfOpt;
+
+
+EX.defaultConfig = adapter.getDefaultConfig();
 
 
 EX.guessMjsFile = function (cjsMod, opt) {
